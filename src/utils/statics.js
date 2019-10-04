b8291dee53d4d6e8759adc010587942e806b5ccb
grigor-aramyan <grigori.aramyan@gmail.com>
2019-10-04 19:53:52 +0400

    add notif and report static types

--- src/utils/statics.js
+++ src/utils/statics.js
@@ -4,3 +4,14 @@ export const baseUri = 'http://localhost:4242';
 export const PM_TYPE = 'PM_TYPE';
 export const DEV_TYPE = 'DEV_TYPE';
 export const ADMIN_TYPE = 'ADMIN_TYPE';
+
+// notif types
+export const TASK_ASSIGNED_NOTIF = 'TASK_ASSIGNED_NOTIF';
+export const DEV_REPORTED_NOTIF = 'DEV_REPORTED_NOTIF';
+export const REPORT_ACCEPTED_NOTIF = 'REPORT_ACCEPTED_NOTIF';
+export const REPORT_REJECTED_NOTIF = 'REPORT_REJECTED_NOTIF';
+
+// report types
+export const UNSEEN_REPORT = 'UNSEEN_REPORT';
+export const REJECTED_REPORT = 'REJECTED_REPORT';
+export const ACCEPTED_REPORT = 'ACCEPTED_REPORT';
