702e5ba4a8591f16a1977127503d4ae814903583
grigor-aramyan <grigori.aramyan@gmail.com>
2019-10-05 00:59:32 +0400

    add update user role redux reducer case

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,8 @@
 import {
     GET_ALL_DEVS,
     GET_ALL_DEVS_AND_PMS,
-    USER_CREATED_FROM_ADMIN
+    USER_CREATED_FROM_ADMIN,
+    UPDATE_USER_ROLE
 } from '../actions/types';
 
 // Statics
@@ -16,6 +17,42 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch (action.type) {
+        case UPDATE_USER_ROLE:
+            const updatedUser = action.paylod;
+
+            const intermediateAllDevs = state.allDevs;
+            let updatedAllDevs = null;
+            if (updatedUser.userType === ADMIN_TYPE) {
+                updatedAllDevs = state.allDevs;
+            } else if (intermediateAllDevs.length > 0) {
+                const filteredAllDevs = intermediateAllDevs.filter(d => {
+                    return (d.id !== updatedUser.id);
+                });
+                filteredAllDevs.unshift(updatedUser);
+                updatedAllDevs = filteredAllDevs;
+            } else {
+                updatedAllDevs = state.allDevs;
+            }
+
+            const intermediateAllDevsAndPms = state.allDevsAndPms;
+            let updatedAllDevsAndPms = null;
+            if (updatedUser.userType === ADMIN_TYPE) {
+                updatedAllDevsAndPms = state.allDevsAndPms;
+            } else if (intermediateAllDevsAndPms.length > 0) {
+                const filteredAllDevs = intermediateAllDevsAndPms.filter(d => {
+                    return (d.id !== updatedUser.id);
+                });
+                filteredAllDevs.unshift(updatedUser);
+                updatedAllDevsAndPms = filteredAllDevs;
+            } else {
+                updatedAllDevsAndPms = state.allDevsAndPms;
+            }
+
+            return {
+                ...state,
+                allDevs: updatedAllDevs,
+                allDevsAndPms: updatedAllDevsAndPms
+            }
         case USER_CREATED_FROM_ADMIN:
             let updatedDevsAndPms = null;
             const newUser = action.payload.user;
